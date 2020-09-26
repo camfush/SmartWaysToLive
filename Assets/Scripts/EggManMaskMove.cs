a0d424ae128676e8a478c01a8c0010a3afa6df58
zcnm <42791139+zcnm@users.noreply.github.com>
2020-09-26 23:20:12 +1000

    Cover your sneeze game start

---
+++ Assets/Scripts/EggManMaskMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EggManMaskMove : MonoBehaviour
+{
+    private BoxCollider2D collider;
+    private bool canMove;
+    public float distance;
+    private Vector3 lastPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        canMove = false;
+        distance = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastPosition = transform.position;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (collider == Physics2D.OverlapPoint(mousePos))
+            {
+                canMove = true;
+            }
+        }
+        if (canMove)
+        {
+            Vector3 position = this.transform.position;
+            position.x = mousePos.x;
+            this.transform.position = position;
+        }
+        distance += Vector3.Distance(transform.position, lastPosition);
+        lastPosition = transform.position;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            canMove = false;
+        }
+    }
+}
---
+++ Assets/Scripts/EggManSneeze.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggManSneeze : SceneController
+{
+    public const double endPoint = -7.2;
+    private GameObject egg, mask;
+    private Animator anim;
+    private void Start()
+    {
+        egg = GameObject.Find("Sneeze Animation");
+        mask = GameObject.Find("EggMask");
+        anim = egg.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(mask.GetComponent<Transform>().localPosition.x == endPoint)
+        {
+
+        }
+       
+    }
+
+    protected override void TimeUp()
+    {
+        anim.Play("EggMan Sneeze");
+
+    }
+
+}
