284df96f4ee6ff4c3b922c9fd0434d0c3d4dd281
Cameron <>
2020-09-26 13:11:49 +1000

    Added Available Lives Logic

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using static GameInfo;
 
 // Parent class for all scene controllers.
 public class SceneController : MonoBehaviour
 {
     public GameObject GameOverlay;
+    public Sprite AvailableLife;
+    public Sprite ConsumedLife;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(GameOverlay);
+        GameInfo.ResetValues();
+        GameObject gameOverlay = Instantiate(GameOverlay);
+        for (int i = 0; i < GameInfo.PlayerLives; i++)
+        {
+            gameOverlay.transform.GetChild(0).GetChild(i).transform.GetComponent<Image>().sprite = AvailableLife;
+        }
     }
 
     protected void PlayerSuccess()
