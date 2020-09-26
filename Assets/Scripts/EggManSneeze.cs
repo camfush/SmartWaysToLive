c0a2d035d6c5cdff429223073c66438f8ab44db3
zcnm <42791139+zcnm@users.noreply.github.com>
2020-09-27 00:07:43 +1000

    Made egg mask guy happy

--- Assets/Scripts/EggManSneeze.cs
+++ Assets/Scripts/EggManSneeze.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EggManSneeze : SceneController
 {
     public const double endPoint = -7.2;
+    public Sprite spriteSuccess;
     private GameObject egg, mask;
     private Animator anim;
     private void Start()
@@ -16,9 +17,15 @@ public class EggManSneeze : SceneController
 
     private void Update()
     {
+
         if(mask.GetComponent<Transform>().localPosition.x <= endPoint+0.5)
         {
-            PlayerSuccess();
+            mask.GetComponent<Transform>().localPosition = egg.GetComponent<Transform>().localPosition;
+            mask.GetComponent<SpriteRenderer>().sprite = spriteSuccess;
+            egg.SetActive(false);
+            StartCoroutine(waiterYes());
+            
+
         }
        
     }
@@ -35,6 +42,11 @@ public class EggManSneeze : SceneController
 
         yield return new WaitForSeconds(1);
         base.TimeUp();
+    }
+    IEnumerator waiterYes()
+    {
+        yield return new WaitForSeconds(1);
+        PlayerSuccess();
     }
 
 }
