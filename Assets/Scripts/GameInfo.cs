e5ccea2514528a8e9a9ba2e0edbf955c14969763
Cameron <>
2020-09-26 12:22:16 +1000

    GameInfo Static Class
    For storing game info

---
+++ Assets/Scripts/GameInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameInfo
+{
+    public static int PlayerLives;
+    public static float TimeMultiplier;
+    public static int PlayerScore;
+
+    static void ResetValues()
+    {
+        PlayerLives = 3;
+        TimeMultiplier = 1;
+        PlayerScore = 0;
+    }
+
+    static void IncScore()
+    {
+        PlayerScore++;
+        if (PlayerScore % 5 == 0)
+        {
+            TimeMultiplier += 0.3f;
+        }
+    }
+}
